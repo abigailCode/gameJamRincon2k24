b71fc23828e23123ada71a6027a029c2b1099607
FlejeCute <abigailluciaojedaalonso@alumno.ieselrincon.es>
2024-04-13 12:37:30 +0100

    camera position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Camera playerCamera;
+
     public float walkSpeed = 6f;
     public float gravity = 10f;
     public float lookSpeed = 2f;
@@ -66,7 +66,6 @@ public class PlayerController : MonoBehaviour
         {
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
     }
