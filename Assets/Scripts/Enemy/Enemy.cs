359edf7ff7bf648d8b8ef24fbb1d767865cb7899
Armas <rubenarmas2010@gmail.com>
2024-04-14 12:18:54 +0100

    Enemy calls DoDamage Summon

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -15,7 +15,8 @@ public class Enemy : MonoBehaviour {
 
     [Header("Damage")]
     [SerializeField] float damagetime = 0.1f;
-    [SerializeField] float amountOfDamage = 10f;
+    [SerializeField] float amountOfDamageToPlayer = 10f;
+    [SerializeField] float amountOfDamageToSummon = 10f;
     private bool canAttack;
     private bool canGetDamage;
 
@@ -28,10 +29,6 @@ public class Enemy : MonoBehaviour {
     public int saludActual;
     public int damageFromPlayer = 10;
 
-    [Header("Booleanos")]
-    public bool tocaJugador;
-    private bool puedeDanar = true;
-
     private void Awake()
     {
         //anim = GetComponent<Animator>();
@@ -39,7 +36,6 @@ public class Enemy : MonoBehaviour {
 
     void Start() {
         saludActual = salud;
-        tocaJugador = false;
         navMeshAgent = GetComponent<NavMeshAgent>();
 
         canAttack = true;
@@ -48,20 +44,6 @@ public class Enemy : MonoBehaviour {
 
 
     void Update() {
-        /*float distanciaEnemyPlayer = Vector3.Distance(transform.position, player.position);
-
-        GameObject playerObject = GameObject.FindWithTag("Player");
-        if (playerObject != null)
-        {
-            Transform playerTransform = playerObject.transform;
-            Vector3 direction = playerTransform.position - transform.position;
-            direction.Normalize();
-            transform.position += direction * speed * Time.deltaTime;
-        }
-        else
-        {
-            Debug.LogWarning("No se encontró al jugador en la escena.");
-        }*/
         SetNavDestination();
 
         // Comprueba si se ha muerto
@@ -70,16 +52,14 @@ public class Enemy : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Summon"))
-            StartCoroutine(RecibirDano());
+            other.GetComponent<InvocationHealthController>().SetDamage(amountOfDamageToSummon);
 
         if (other.CompareTag("Player"))
-            StartCoroutine(DamageToPlayer(amountOfDamage));
+            StartCoroutine(DamageToPlayer(amountOfDamageToPlayer));
     }
 
     private void OnTriggerExit(Collider other) {
-        StopCoroutine(RecibirDano());
-
-        StopCoroutine(DamageToPlayer(amountOfDamage));
+        StopCoroutine(DamageToPlayer(amountOfDamageToPlayer));
     }
 
     public void RecibeDano(int dano)
@@ -114,18 +94,6 @@ public class Enemy : MonoBehaviour {
                 closestDistance = distance;
             }
         }
-    }
-
-    IEnumerator RecibirDano() {
-        while (true) {
-            if (canGetDamage)
-                RecibeDano(damageFromPlayer);
-
-            canGetDamage = false;
-            yield return new WaitForSeconds(damagetime);
-            canGetDamage = true;
-        }
-        
     }
 
     IEnumerator DamageToPlayer(float damage) {
